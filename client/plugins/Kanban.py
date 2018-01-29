f5c58db13ce7f78b86c8b0789e35e27305128cc0
witcare <pm@witcare.com>
2018-01-29 18:55:02 +0800

    添加页面导航声音提示

--- client/plugins/Kanban.py
+++ client/plugins/Kanban.py
@@ -24,9 +24,12 @@ def handle(text, mic, profile, wxbot=None):
                    number)
         wxBot -- wechat robot
     """
-
+    if any(ext in text for ext in [u"首页", u"返回首页", u"开机页面"]):
+        sendSocket('0000')
+        mic.say(u"即将返回开机页面")
     if any(ext in text for ext in [u"看板", u"生产看板", u"异常看板"]):
         sendSocket('0101')
+        mic.say(u"即将打开看板页面")
         #mic.say(u"01看板看板")
     if any(ext in text for ext in [u"上一页", u"上页", u"上", u"往上", u"前", u"往前"]):
         sendSocket('0102')
@@ -35,12 +38,16 @@ def handle(text, mic, profile, wxbot=None):
         sendSocket('0103')
     if any(ext in text for ext in [u"报工", u"生产报工"]):
         sendSocket('0201')
+        mic.say(u"即将打开报工页面")
     if any(ext in text for ext in [u"缺料", u"生产缺料"]):
         sendSocket('0301')
+        mic.say(u"缺料信息已经提交，将发送短信给相关负责人，并打开报警灯，需要关闭报警灯，请按取消按键")
     if any(ext in text for ext in [u"设备故障", u"设备报修"]):
         sendSocket('0401') 
+        mic.say(u"设备报修通知已经提交，将发送短信给相关负责人，并打开报警灯，需要关闭报警灯，请按取消按键")
     if any(ext in text for ext in [u"作业指导书"]):
-        sendSocket('0501')                    
+        sendSocket('0501')   
+         mic.say(u"即将打开作业指导书页面")                 
         #mic.say(u"03看板下一页")
         
     #mic.say(text)
@@ -53,4 +60,4 @@ def isValid(text):
         Arguments:
         text -- user-input, typically transcribed speech
     """
-    return any(word in text for word in ["看板","生产看板","异常看板","上一页", "下一页","上页", "下页", "报工", "生产报工", "缺料", "设备故障", "设备报修", "作业指导书", "上", "下", "前", "后", "往上", "往下", "往前", "往后"])
+    return any(word in text for word in ["首页","返回首页","开机页面","看板","生产看板","异常看板","上一页", "下一页","上页", "下页", "报工", "生产报工", "缺料", "设备故障", "设备报修", "作业指导书", "上", "下", "前", "后", "往上", "往下", "往前", "往后"])
